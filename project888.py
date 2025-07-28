385da72163c13d22c0fb77b3e6ad9102dd6891ae
Kristine <kazarankristine07@gmail.comgit config --global user.email kazarankristine07@gmail.com git config --global user.name Kristinegit config --listgit config --global user.email kazarankristine07@gmail.com>
2025-07-28 19:44:30 +0400

    first

---
+++ project888.py
@@ -0,0 +1,93 @@
+num = input("choose the template ")
+print(num)
+if num == "1":
+    a = input("number")
+    b = input("time")
+    c = input("transport")
+    d = input("adjective")
+    e = input("adjective2")
+    e1 = input("noun")
+    f = input("color")
+    j = input("part of the body")
+    h = input("verb")
+    i = input("number2")
+    k = input("noun2")
+    l = input("noun3")
+    m = input("part of the body2")
+    n = input("verb")
+    o = input("noun4")
+    p = input("adjective3")
+    q = input("silly word")
+    r = input("noun")
+
+    text = (f"It was about {a} {b} ago when I arrived at the hospital in a {c}. "
+            f"The hospital is a/an {d} place, there are a lot of {e} {e1} here. "
+            f"There are nurses here who have  {f} {j}. "
+            f"If someone wants to come into my room I told them that they have to {h} first. "
+            f"I’ve decorated my room with {i} {k}. "
+            f"Today I talked to a doctor and they were wearing a {l} on their {m}. "
+            f"I heard that all doctors {n} {o} every day for breakfast. "
+            f"The most {p} thing about being in the hospital is the {q} {r} !")
+
+
+
+    print(text)
+elif num =="2":
+    a = input("Proper Noun (Person’s Name) ")
+    b = input("Noun")
+    c = input("Adjective (Feeling)")
+    d = input("Verb")
+    e = input("Adjective (Feeling) 2")
+    e1 = input("Animal")
+    f = input("Verb2")
+    j = input("Color")
+    h = input("Verb (ending in ing)")
+    i = input("Adverb (ending in ly)")
+    k = input("Number")
+    l = input("Measure of Time")
+    m = input("Color")
+    n = input("Animal")
+    o = input("Number")
+    p = input("Silly Word")
+    r = input("noun2")
+    text = (f"This weekend I am going camping with {a}. "
+            f"I packed my lantern, sleeping bag, and {b}. I am so {c} to {d} in a tent. "
+            f"I am {e} we might see a(n) {e1}, I hear they’re kind of dangerous. "
+            f"While we’re camping, we are going to hike, fish, and {f}. "
+            f"I have heard that the {j} lake is great for {h}. "
+            f"Then we will  {i} hike through the forest for {k} {l}. "
+            f"If I see a {m} {n} while hiking, I am going to bring it home as a pet!"
+            f" At night we will tell {o} {p} stories and roast {r} around the campfire!!")
+
+    print(text)
+
+elif num =="3":
+    a = input("Proper Noun (Person’s Name) ")
+    b = input("Adjective")
+    c = input("Color")
+    d = input("Animal")
+    e = input("Place")
+    e1 = input("Adjective2")
+    f = input("Magical Creature (Plural")
+    j = input("Adjective3")
+    h = input("Magical Creature (Plural)2")
+    i = input("Room in a House")
+    k = input("Noun")
+    l = input("Noun2")
+    m = input("Noun(Plural)3")
+    n = input("Adjective4")
+    o = input("Noun (Plural)4")
+    p = input("Number")
+    r = input("Measure of time")
+    q = input("Verb (ending in ing)")
+    s = input("Adjective5")
+    t = input("Noun5")
+    text = (f" Dear {a},"
+            f" I am writing to you from a {b} castle in an enchanted forest. "
+            f"I found myself here one day after going for a ride on a {c} {d} in {e}. "
+            f"There are {e1} {f} and {j} {h} here! "
+            f"In the {i} there is a pool full of {k}. "
+            f"I fall asleep each night on a {l} of {m} and dream of {n} {o}."
+            f" It feels as though I have lived here for {p} {r}. "
+            f"I hope one day you can visit, although the only way to get here now is {q} on a {s} {t}!!")
+    print(text)
